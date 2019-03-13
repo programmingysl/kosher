a906d1714a723311422cc621ff25d841814aa539
unknown <programmingysl@gmail.com>
2019-03-13 13:37:58 -0400

    added card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kosher';
+  subtitle = 'this is subtitle';
+  content = 'this is a random content paragraph text';
+
+
+  // expected because of top implements Oninit
+  ngOnInit(): void {
+    this.title = 'Word !!';
+  }
+
 }
